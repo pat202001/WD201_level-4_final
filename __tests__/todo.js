2d814b2e23d9c193de672bd2b4cb1d0e2cb314c6
rwandantechy <niyinnocent2027@.com>
2022-11-18 23:33:46 +0530

    adding new comments in todo file

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -2,8 +2,9 @@
 /* eslint-disable no-undef */
 const todoList = require("../todo");
 // eslint-disable-next-line no-unused-vars
+// adding all , add and markAsComplete functions.
 const { all, add, markAsComplete } = todoList();
-
+// testing starts here
 describe("Testing my todo app:", () => {
   beforeAll(() => {
     add({
@@ -12,7 +13,7 @@ describe("Testing my todo app:", () => {
       dueDate: new Date().toLocaleDateString("en-CA"),
     });
   });
-
+  // second test starts for adding new todo`
   test("Should add new todo ", () => {
     const todoItemCount = all.length;
     add({
@@ -23,15 +24,17 @@ describe("Testing my todo app:", () => {
 
     expect(all.length).toBe(todoItemCount + 1);
   });
-
+  //third test starts here.
   test("Should mark todo  as complete", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
   });
+  // fourth test starts here
   test("Should retrieve today items", () => {
     expect(all.dueToday).toBe(all.dueDate);
   });
+  // the last test starts here
   test("Should retrieve overdue items.", () => {
     expect(all.dueDate).toBe(all.overdue);
   });
